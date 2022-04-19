202023d24bda8987c31f5b0abca9befd2309d834
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-19 16:58:31 +0000

    fix game.js

--- js/game.js
+++ js/game.js
@@ -19,8 +19,8 @@ const config = {
     }
   },
   // set background color
-  backgroundColor: 0x5f6e7a
-   scale: {
+  backgroundColor: 0x5f6e7a,
+  scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH
