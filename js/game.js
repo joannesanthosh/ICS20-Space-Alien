ea5ca4fa025d03962ce21af4708fa44935e5eda8
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-19 16:52:53 +0000

    added comments to files

--- js/game.js
+++ js/game.js
@@ -1,10 +1,30 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Apr 2022
+// This is the Phaser3 configuration file
+
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO, 
   width: 1920,
   height: 1080,
+   physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
   backgroundColor: 0x5f6e7a
+   scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
