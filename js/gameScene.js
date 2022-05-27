aea80b47bfb8962a8fb7c1a662d6eb923ffce838
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-27 16:34:12 +0000

    update gamescene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -30,6 +30,9 @@ class GameScene extends Phaser.Scene {
     this.background = null
     this.ship = null
     this.fireMissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = {font: "65px Ariel", fill: "#ffffff", align: "center",}
   }
 
   /**
@@ -39,7 +42,7 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("#0x5f6e7a")
   }
 
   /**
@@ -67,6 +70,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     // create a group for the missiles
@@ -81,6 +86,8 @@ class GameScene extends Phaser.Scene {
       alienCollide.destroy()
       missileCollide.destroy()
       this.sound.play('explosion')
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
       this.createAlien()
       this.createAlien()
     }.bind(this))
